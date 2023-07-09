f52e2af892ebdd401221441a8db90b6e62b395bb
Hasan Polat <thatsnotpublic@gmail.com>
2023-07-09 19:37:51 +0300

    feat: added login to reddit

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Page
 from bs4 import BeautifulSoup
 import time
 
 BASE_URL = "https://www.reddit.com/r/"
 SUBREDDIT_NAME = "midjourney/new"
+LOGIN_URL = "https://www.reddit.com/login"
+USERNAME = "gwynbleiddww"
+PASSWORD = "Hasan134679258."
+
+def login_to_reddit(page: Page) -> Page:
+    page.goto(f"{LOGIN_URL}")
+
+    page.fill('input[name="username"]', USERNAME)
+    page.fill('input[name="password"]', PASSWORD)
+    page.click('button[type="submit"]')
+
+    return page
 
 def crawl_subreddit():
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=True)
+        browser = playwright.chromium.launch(headless=False, slow_mo=1000)
         page = browser.new_page()
+
+        login_to_reddit(page)
 
         page.goto(f"{BASE_URL}{SUBREDDIT_NAME}")
         content = page.content()
@@ -17,15 +31,15 @@ def crawl_subreddit():
 
         titles = soup.findAll("h3")
         authors = soup.findAll(attrs={"data-testid": "post_author_link"})
-        posts = soup.findAll(attrs={"data-click-id": "text"})
+        posts = soup.findAll(attrs={"data-click-id": "media"})
 
         while True:
             for title, author, post in zip(titles, authors, posts):
-                print(f"author: ", author.text.encode('utf-8').decode('utf-8'), flush=True)
-                print(f"title: ",title.text.encode('utf-8').decode('utf-8'), flush=True)
-                print("post: ", post.text.encode('utf-8').decode('utf-8'), flush=True)
+                print(f"author: ", author.text)
+                print(f"title: ", title.text)
+                print(f"post: ", post.text)
 
-            time.sleep(5)
+            time.sleep(4)
             page.reload()
 
         browser.close()
