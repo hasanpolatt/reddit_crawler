45f16bdd5b41e994af6930ac9e22edef3946cf35
Hasan Polat <thatsnotpublic@gmail.com>
2023-07-10 13:30:05 +0300

    refactor: refactoring main.py and added gitignore

--- main.py
+++ main.py
@@ -1,47 +1,45 @@
-from playwright.sync_api import sync_playwright, Page
 from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright, Page
 import time
 
 BASE_URL = "https://www.reddit.com/r/"
 SUBREDDIT_NAME = "midjourney/new"
 LOGIN_URL = "https://www.reddit.com/login"
-USERNAME = "gwynbleiddww"
-PASSWORD = "Hasan134679258."
+USERNAME = "username"
+PASSWORD = "password"
 
-def login_to_reddit(page: Page) -> Page:
-    page.goto(f"{LOGIN_URL}")
+class RedditCrawler:
 
-    page.fill('input[name="username"]', USERNAME)
-    page.fill('input[name="password"]', PASSWORD)
-    page.click('button[type="submit"]')
+    def login(self, page: Page):
+        page.goto(f"{LOGIN_URL}", wait_until="load")
 
-    return page
+        page.fill('input[name="username"]', USERNAME)
+        page.fill('input[name="password"]', PASSWORD)
+        page.click('button[type="submit"]')
 
-def crawl_subreddit():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False, slow_mo=1000)
-        page = browser.new_page()
+        return page
 
-        login_to_reddit(page)
-
-        page.goto(f"{BASE_URL}{SUBREDDIT_NAME}")
-        content = page.content()
-
+    def crawl(self, content: str):
         soup = BeautifulSoup(content, 'html.parser')
 
         titles = soup.findAll("h3")
         authors = soup.findAll(attrs={"data-testid": "post_author_link"})
         posts = soup.findAll(attrs={"data-click-id": "media"})
 
-        while True:
-            for title, author, post in zip(titles, authors, posts):
-                print(f"author: ", author.text)
-                print(f"title: ", title.text)
-                print(f"post: ", post.text)
+        for title, author, post in zip(titles, authors, posts):
+            print(f"author: ", author.text)
+            print(f"title: ", title.text)
+            print(f"post: ", post.text)
 
-            time.sleep(4)
-            page.reload()
+    def run(self):
+        with sync_playwright() as pw:
+            browser = pw.chromium.launch(headless=False, slow_mo=300)
+            page = browser.new_page()
 
-        browser.close()
+            self.login(page)
 
-crawl_subreddit()
+            page.goto(f"{BASE_URL}{SUBREDDIT_NAME}", wait_until="load")
+
+            self.crawl(page.content())
+
+RedditCrawler().run()
