4946a8f5a4bd1c6a3181ee82f39c19e618785a80
Hasan Polat <thatsnotpublic@gmail.com>
2023-07-10 16:53:42 +0300

    feat: posts are saved to database now

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import psycopg2
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright, Page
 import time
@@ -5,10 +6,30 @@ import time
 BASE_URL = "https://www.reddit.com/r/"
 SUBREDDIT_NAME = "midjourney/new"
 LOGIN_URL = "https://www.reddit.com/login"
-USERNAME = "username"
-PASSWORD = "password"
+USERNAME = "gwynbleiddww"
+PASSWORD = "Anotherreddit6547."
 
 class RedditCrawler:
+
+    def connect_to_database(self):
+        connection = psycopg2.connect(
+            host="127.0.0.1",
+            database="postgres",
+            user="hasan",
+            password="123123"
+        )
+        return connection
+
+    def save_post_to_database(self, connection, title, author, post):
+        cursor = connection.cursor()
+
+        query = "INSERT INTO posts (title, author, post) VALUES (%s, %s, %s)"
+        values = (title, author, post)
+
+        cursor.execute(query, values)
+        connection.commit()
+
+        cursor.close()
 
     def login(self, page: Page):
         page.goto(f"{LOGIN_URL}", wait_until="load")
@@ -22,6 +43,8 @@ class RedditCrawler:
     def crawl(self, content: str):
         soup = BeautifulSoup(content, 'html.parser')
 
+        connection = self.connect_to_database()
+
         titles = soup.findAll("h3")
         authors = soup.findAll(attrs={"data-testid": "post_author_link"})
         posts = soup.findAll(attrs={"data-click-id": "media"})
@@ -29,11 +52,13 @@ class RedditCrawler:
         for title, author, post in zip(titles, authors, posts):
             print(f"author: ", author.text)
             print(f"title: ", title.text)
-            print(f"post: ", post.text)
+            print(f"post: ", post.text, "\n")
+            # print("--------------------------------------------------------------------")
+            self.save_post_to_database(connection, title.text, author.text, post.text)
 
     def run(self):
         with sync_playwright() as pw:
-            browser = pw.chromium.launch(headless=False, slow_mo=300)
+            browser = pw.chromium.launch(headless=False, slow_mo=3000)
             page = browser.new_page()
 
             self.login(page)
